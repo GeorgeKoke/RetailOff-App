e425d0d2ce7add08c8b675e670c50d58a7d0838f
GeorgeKoke <jorge.contreras.palma@gmail.com>
2023-05-14 05:29:57 -0400

    Init Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Retail-App-v2';
+  public appPages =[
+    {title: 'Home', url: '/home', icon: 'home'},
+    {title: 'Dashboard', url: '/dashboard', icon: 'pie-chart'},
+    {title: 'Login', url: '/login', icon: 'log-in'},
+    {title: 'Register', url: '/user-register', icon: 'person-add'},
+  ];
+  constructor(){}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { UserRegisterComponent } from './pages/user-register/user-register.component';
+import { IonicModule } from '@ionic/angular';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    DashboardComponent,
+    UserRegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    IonicModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
